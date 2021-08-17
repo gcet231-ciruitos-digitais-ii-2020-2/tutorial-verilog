babc72e8c33eafa5976a3b39a94d5971db2b0fa1
joaocarlos <joaocarlos@ufrb.edu.br>
2021-08-17 14:30:50 -0300

    Circuito somador completo.

--- rtl/onebitadder.v
+++ rtl/onebitadder.v
@@ -5,4 +5,14 @@ module onebitadder (
   output sum, cout
 );
 
+wire a_b_xor, a_cin_and, b_cin_and, a_b_and;
+
+xor (a_b_xor, a, b),
+    (sum, a_b_xor, cin);
+
+and (a_cin_and, a, cin),
+    (b_cin_and, b, cin),
+    (a_b_and, a, b);
+
+or (cout, a_cin_and, b_cin_and, a_b_and);
 endmodule
