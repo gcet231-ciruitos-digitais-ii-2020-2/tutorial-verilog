5eaba88b7ad451e8b79785fadbde3c29cdf8e402
joaocarlos <joaocarlos@ufrb.edu.br>
2021-08-23 17:26:45 -0300

    Arquivo top.v para o Tutorial 6.

---
+++ rtl/top.v
@@ -0,0 +1,19 @@
+// Tutorial 6 - Datapath completo.
+
+module top #(
+   parameter WIDTH = 8
+   )(
+   input clk,
+   input rst,
+   input [5:0] cmdin,
+   input [WIDTH-1:0] din_1,
+   input [WIDTH-1:0] din_2,
+   input [WIDTH-1:0] din_3,
+   output [WIDTH-1:0] dout_low,
+   output [WIDTH-1:0] dout_high,
+   output zero,
+   output error
+);
+
+
+endmodule
